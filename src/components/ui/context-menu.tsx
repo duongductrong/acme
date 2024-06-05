fd5bd94f1f4066266467535749b293d9a491d9d5
Duong Duc Trong <duongductrong06@gmail.com>
2024-06-05 12:24:14 +0700

    chore: update path of tailwind util

--- src/components/ui/context-menu.tsx
+++ src/components/ui/context-menu.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import * as ContextMenuPrimitive from "@radix-ui/react-context-menu"
 import { CheckIcon, ChevronRightIcon, DotFilledIcon } from "@radix-ui/react-icons"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/tailwind"
 
 const ContextMenu = ContextMenuPrimitive.Root
 
